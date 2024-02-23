e098d5723bc2b1f472903436af5e47f27c79166e
avknight <160674714+avknight@users.noreply.github.com>
2024-02-22 22:30:20 -0500

    updated html and started js code

---
+++ src/index.js
@@ -0,0 +1,7 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+}
+
+let searchFormelement = document.querySelector("#search-form");
+searchFormelement.addEventListener("submit", handleSearchSubmit);
