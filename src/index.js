7845fb7cb44838e09b43eb4c469390607bd9630d
avknight <160674714+avknight@users.noreply.github.com>
2024-02-23 11:07:39 -0500

    axios updaate

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apikey = "9t5b364c049odb68c86a10ac1444f04e";
+  let apiUrl =
+    '"https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric"';
+}
+console.log(apiUrl);
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormelement = document.querySelector("#search-form");
