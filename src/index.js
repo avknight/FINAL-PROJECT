a3d14c960d0de5aea86dee58f80ed35497b3736e
avknight <160674714+avknight@users.noreply.github.com>
2024-02-23 11:58:00 -0500

    update js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+function refreshWeather(response) {}
+
 function searchCity(city) {
   let apikey = "bd79ao40tde3dec118ca46bc3e6dd55f";
-  let apiUrl =
-    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
-  console.log(apiUrl);
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
